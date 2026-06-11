66d0ea0bb967d59d6dfb0f05bfd0bc6cd7b98849
otahiri <chucy0066@gmail.com>
2026-06-11 17:07:39 +0100

    feat: started to work on a nn

--- neural_network.py
+++ neural_network.py
@@ -7,27 +7,22 @@ from typing import Sequence
 import numpy as np
 
 
-def segmoid(x):
-    return 1 // (1 + np.exp(-x))
-
-def  derivative_seg(x):
-    return x * (1 - x)
-
-def main() -> None:
-    """the entry point for the program"""
-    neuron = 8
-    
-    input = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    output = np.array([[0], [1], [1], [0]])
-    w1 = np.random.randn(2, 8)
-    b1 = np.random.randn(1, 8)
-    w2 = np.random.randn(8, 1)
-    b2 = np.random.randn(1, 1)
-    hidden_linear = input @ w1 + b1
-    hidden_activated = segmoid(hidden_linear)
-    seg_linear = hidden_activated @ w2 + b2
-    output = segmoid(seg_linear)
-    print(output)
+def main():
+    input_data = np.array([[ 1, 2, 3, 2.5 ],
+                           [1, 2, 3, 2.5]])
+    weight = np.array([[
+            [0.2, 0.8, -0.5, 1.0],
+            [0.5, -0.91, 0.26, -0.5],
+            [-0.26, 0.27, 0.17, 0.87]
+          ],
+                       [
+            [0.2, 0.8, -0.5, 1.0],
+            [0.5, -0.91, 0.26, -0.5],
+            [-0.26, 0.27, 0.17, 0.87]
+          ]])
+    biases = np.array([2, 3, 0.5])
+    layer_output = np.dot(weight, input_data) + biases
+    print(layer_output.shape)
 
 
 if __name__ == "__main__":
