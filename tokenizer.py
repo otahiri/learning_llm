4ab32604bc33c253d2ae600c1a39d8dbf007fb9a
otahiri <chucy0066@gmail.com>
2026-06-14 14:45:06 +0100

    feat: implemented tokenizer using vocab and merge file from call me maybe project

--- tokenizer.py
+++ tokenizer.py
@@ -1,89 +1,76 @@
-# vocab = "/home/otahiri-/.cache/huggingface/hub/models--Qwen--Qwen3-0.6B/snapshots/c1899de289a04d12100db370d81485cdf75e47ca/vocab.json"
-# merges = "/home/otahiri-/.cache/huggingface/hub/models--Qwen--Qwen3-0.6B/snapshots/c1899de289a04d12100db370d81485cdf75e47ca/merges.txt"
+vocab_file = "/home/otahiri-/.cache/huggingface/hub/models--Qwen--Qwen3-0.6B/snapshots/c1899de289a04d12100db370d81485cdf75e47ca/vocab.json"
+merge_file = "/home/otahiri-/.cache/huggingface/hub/models--Qwen--Qwen3-0.6B/snapshots/c1899de289a04d12100db370d81485cdf75e47ca/merges.txt"
 # tokenizer = "/home/otahiri-/.cache/huggingface/hub/models--Qwen--Qwen3-0.6B/snapshots/c1899de289a04d12100db370d81485cdf75e47ca/tokenizer.json"
-# special_chars = {' ' : 'Ġ', "\n" : 'Ċ', '\t' : 'ĉ'}
+special_chars = {" ": "Ġ", "\n": "Ċ", "\t": "ĉ"}
 
+import json
 
-vocab_cap = 1000
 new_token = 256
 merges: dict = dict()
 
 
 def decode(token: int, translation: dict) -> str:
-    if token < 256:
-        return chr(token)
-    else:
-        return translation[token]
+    return translation.get(token, "")
 
 
-def encode(text: str):
+def encode(text: str, vocab: dict):
 
-    tokens = list(text.encode('utf-8'))
-
+    tokens = [vocab.get(c, 0) for c in text]
     while True:
         pairs = [(tokens[i], tokens[i + 1]) for i in range(len(tokens) - 1)]
         pair_to_merge = None
-        lowest_id = float('inf')
+        lowest_rank = float('inf')
+        merge_id = None
         for p in pairs:
-            if p in merges and merges[p] < lowest_id:
-                pair_to_merge = p
-                lowest_id = merges[p]
+            if p in merges:
+                target_id, rank = merges[p]
+                if rank < lowest_rank:
+                    lowest_rank = rank
+                    pair_to_merge = p
+                    merge_id = target_id
 
         if not pair_to_merge:
             break
-        new_tokens: list = []
         i = 0
         while i < len(tokens) - 1:
             if (tokens[i], tokens[i + 1]) == pair_to_merge:
-                tokens[i:i+2] = [lowest_id]
+                tokens[i: i + 2] = [merge_id]
             else:
                 i += 1
     return tokens
 
 
-def update_vocab(encoded: list, pairs: list, vocab: set, translation: dict):
-    global new_token
-    pair, count = pairs[0]
-    if count > 1 and len(vocab) < vocab_cap:
-        vocab.add("".join([decode(x, translation) for x in pair]))
-        i = 0
-        translation[new_token] = "".join([decode(x, translation) for x in pair])
-        merges[pair] = new_token
-        while i < len(encoded) - 1:
-            if (encoded[i], encoded[i + 1]) == pair:
-                encoded[i: i + 2] = [new_token]
-            else:
-                i += 1
-        new_token += 1
-
-
-def count_pairs(text: list) -> list:
-
-    count: dict = dict()
-
-    i = 0
-    while i < len(text) - 1:
-        count[(text[i], text[i + 1])] = count.get((text[i], text[i + 1]), 0) + 1
-        i += 1
-    return sorted(count.items(), key=lambda x: x[1], reverse=True)
-
-
 def main() -> None:
-    text = "hellor world! howhow howare howhhhhhhow you doing or, or or or or"
+    global new_token
 
-    vocab: set = set(text)
+    vocab: dict = dict()
+    with open(vocab_file, "r") as f:
+        vocab = dict(json.load(f))
+    text = "hello world, my name is oussama"
+    with open(merge_file, "r") as f:
+        lines = f.read().splitlines()
+        for rank, line in enumerate(lines):
+            if not line or line.startswith("#"):
+                continue
+            parts = line.split(' ')
+            if len(parts) == 2:
+                p1, p2 = parts
+                merge_str = p1 + p2
+                if p1 in vocab and p2 in vocab and merge_str in merges:
+                    merges[(vocab[p1], vocab[p2])] = (vocab[merge_str], rank)
+            new_token += 1
 
-    encoded = list(text.encode("utf-8"))
+    for key, value in special_chars.items():
+        text = text.replace(key, value)
 
-    translation: dict = {list(a.encode("utf-8"))[0]: a for a in vocab}
-    while True:
-        pairs = count_pairs(encoded)
-        if pairs and pairs[0][1] < 2:
-            break
-        update_vocab(encoded, pairs, vocab, translation)
-    print(translation)
-    print(encode(text))
-    print("".join([decode(x, translation) for x in encode(text)]))
+    str_to_id = {v: k for k, v in vocab.items()}
+    encoded = encode(text, vocab)
+    print(encoded)
+    text = "".join([decode(x, str_to_id) for x in encoded])
+
+    for key, value in special_chars.items():
+        text = text.replace(value, key)
+    print(text)
 
 
 if __name__ == "__main__":
