534c793edfbe409073a80099da29b6dfa4239a93
otahiri <chucy0066@gmail.com>
2026-06-29 12:15:03 +0100

    feat: added the initial setup for neural network with backpropogation

--- nn_zero_to_hero/__main__.py
+++ nn_zero_to_hero/__main__.py
@@ -1,13 +1,40 @@
-from nn_zero_to_hero import Value
+from nn_zero_to_hero import Value, value
 
 
 def main():
-    a = Value(29.0, label='a')
-    b = Value(2.0, label='b')
-    d = a + b
-    print(d)
-    print(d)
-    print(d._op)
+    x1 = Value(2.0, label="x1")
+    x2 = Value(14.2, label="x2")
+
+    w1 = Value(2.0, label="w1")
+    w2 = Value(1.0, label="w2")
+    b = Value(3.0, label="b")
+
+    target = Value(5.92, label="target")
+    x1w1 = Value(0, label="x1w1")
+    x2w2 = Value(0, label="x2w2")
+    x1w1x2w2 = Value(0, label="x1w1x2w2")
+    n = Value(0, label="n")
+    o = Value(1, label="o")
+
+    for step in range(10000):
+        x1w1 = x1 * w1
+        x2w2 = x2 * w2
+        x1w1x2w2 = x1w1 + x2w2
+        n = x1w1x2w2 + b
+        o = n.ReLu()
+        diff = o - target
+        loss = diff * diff
+        loss.backwards()
+        w1.data -= 0.1 * w1.grad
+        w2.data -= 0.1 * w2.grad
+        b.data -= 0.1 * b.grad
+
+        w1.grad = 0.0
+        w2.grad = 0.0
+        b.grad = 0.0
+        if step%100 == 0:
+            print(o)
+
 
 
 if __name__ == "__main__":
--- nn_zero_to_hero/value.py
+++ nn_zero_to_hero/value.py
@@ -1,55 +1,56 @@
-from graphviz import Digraph
-class Value():
-    def __init__(self, data, _children=(), _op='', label='') -> None:
+class Value:
+    def __init__(self, data, _children=(), _op="", label="") -> None:
         self.data = data
-        self._prev = set(_children)
+        self.grad = 0.0
+        self._prev = _children
         self._op = _op
         self.label = label
 
     def __repr__(self) -> str:
-        return (f'Value(data={self.data})')
+        return f"Value(data={self.data})"
 
     def __add__(self, other):
         return Value(self.data + other.data, (self, other), "+")
 
     def __mul__(self, other):
-        return Value(self.data * other.data, (self, other), '*')
+        return Value(self.data * other.data, (self, other), "*")
 
     def __sub__(self, other):
-        return Value(self.data - other.data, (self, other), '-')
+        other = other if isinstance(other, Value) else Value(other)
+        return self + (other * Value(-1.0))
 
+    def ReLu(self):
+        out = Value(max(0.001 * self.data, self.data), (self,), "ReLu")
+        return out
 
-def trace(root):
-    """build  trace trail for root with all its component
+    def __pow__(self, pow):
+        return Value(self.data ** pow, (self,), "pow")
 
-    Args:
-        root (Value): the root Value object
+    def backwards(self):
+        self.grad = 1.0
+        visited = set()
+        topo = []
 
-    Returns:
-        tuple of nodes and edges that make root
-    """
-    nodes, edges = set(), set()
+        def build_links(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_links(child)
+                topo.append(v)
 
-    def build(v):
-        if v not in nodes:
-            nodes.add(v)
-            for child in v._prev:
-                edges.add((child, v))
-                build(child)
-    build(root)
-    return nodes, edges
+        build_links(self)
+        for node in reversed(topo):
+            children = list(node._prev)
 
+            match node._op:
+                case "ReLu":
+                    children[0].grad += (
+                        1.0 if children[0].data >= 0 else 0.001
+                    ) * node.grad
+                case "+":
+                    for child in children:
+                        child.grad += node.grad
 
-def draw_dot(root):
-    dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'})
-    nodes, edges = trace(root)
-    for n in nodes:
-        uid = str(id(n))
-        dot.node(name=uid, label='{ data %.4f }' % (n.data), shape='record')
-        if n._op:
-            dot.node(name=uid + n._op, label=n._op)
-            dot.edge(uid + n._op, uid)
-    for n1, n2 in edges:
-        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
-    return dot
-
+                case "*":
+                    children[1].grad += children[0].data * node.grad
+                    children[0].grad += children[1].data * node.grad
